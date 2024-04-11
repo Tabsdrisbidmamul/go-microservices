44c5a83ff75d0ec937a7f1ab12cca0036cc40007
Idris Khan <idris.khan303@gmail.comm>
2024-04-11 16:07:08 +0100

    Added certs and basic custom server

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
-	
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Customer service")
+	})
+
+	server := http.Server {
+		Addr: ":3000",
+	}
+
+	go func() {
+		log.Fatal(server.ListenAndServeTLS("./cert.pem", "key.pem"))
+	}()
+
+	fmt.Println("Server started, press <Enter> to shutdown")
+	fmt.Scanln()
+
+	server.Shutdown(context.Background())
+	fmt.Println("Server stopped")
 }
