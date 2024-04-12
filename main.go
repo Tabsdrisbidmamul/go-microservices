0b40e25f616f04e6bb9caf3d5b129816bcadfdb9
Idris Khan <ricepoo123@gmail.com>
2024-04-12 15:41:24 +0100

    Covered basics of go http

--- main.go
+++ main.go
@@ -5,12 +5,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Customer service")
-	})
+	http.Handle("/", customHandler("Customer service"))
+
+	var handlerFunc http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, r.URL.String())
+	}
+
+	http.HandleFunc("/url", handlerFunc)
+
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintln(w, "Customer service")
+	// })
 
 	server := http.Server {
 		Addr: ":3000",
@@ -25,4 +34,19 @@ func main() {
 
 	server.Shutdown(context.Background())
 	fmt.Println("Server stopped")
+}
+
+type customHandler string
+
+func (cs customHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("X-Powered-By", "gophers")
+	http.SetCookie(w, &http.Cookie{
+		Name: "session-id",
+		Value: "12345",
+		Expires: time.Now().Add(24 * time.Hour * 365), // expire in a year
+	})
+	w.WriteHeader(http.StatusAccepted)
+
+	fmt.Fprintln(w, string(cs))
+	fmt.Fprintln(w, r.Header)
 }
